1b124769dd8c6e9c8100448b039b33bc0413db8d
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-08-30 20:15:54 +0700

    20/08

---
+++ src/AccountTest.java
@@ -0,0 +1,18 @@
+public class AccountTest {
+    private static Account accountExample;
+
+    public static final String[] validAccount = new String[] { "123abc_", "_abc123", "______", "123456","abcdefgh" };
+    public static final String[] invalidAccount = new String[] { ".@", "12345", "1234_", "abcde" };
+
+    public static void main(String[] args) {
+        accountExample = new Account();
+        for (String account : validAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Tai khoan la " + account +" hop le: "+ isValid);
+        }
+        for (String account : invalidAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Tai khoan la " + account +" hop le: "+ isValid);
+        }
+    }
+}
